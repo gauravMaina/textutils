b5d4d27abb8792928c00113d9cabfddb66efe97d
gauravMaina <gauravjadhav839@gmail.com>
2022-10-24 19:58:07 +0530

    lowercase button and preview added

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,14 +1,18 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function TextArea() {
-    const[text,setText]= useState('');
-const handleOnChange = (event)=>{
-setText(event.target.value);
-}
-const handleOnClick = ()=>{
+  const [text, setText] = useState("");
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleUpClick = () => {
     const newText = text.toUpperCase();
     setText(newText);
-}
+  };
+  const handleLowClick = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  };
   return (
     <div className="container">
       <div className="mb-3">
@@ -21,7 +25,20 @@ const handleOnClick = ()=>{
           onChange={handleOnChange}
         />
       </div>
-      <button className="btn btn-primary" onClick={handleOnClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+        Convert to UpperCase
+      </button>
+      <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+        Convert to LowerCase
+      </button>
+      <div className="container">
+        <h2>
+          {text.split(" ").length} Words and {text.length} Characters
+        </h2>
+        <p>{0.008 * text.split(" ").length} mins to read</p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
